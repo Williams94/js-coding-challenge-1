e0f28dc0bbd102ee854a89a442c7133974f37ee7
Ross <ross@Ross-W-MacBook-Pro.local>
2017-09-16 11:43:48 +0100

    First 4 tests of test 3 passing

--- app/test3.js
+++ app/test3.js
@@ -1,3 +1,13 @@
 export default function test3(numMax) {
-
+    let sequence = [];
+    if (numMax !== undefined && numMax !== 0) {
+        for (let i = 0; i < numMax; i++) {
+            let prev = 0;
+            sequence.push(prev + i);
+            prev = i;
+        }
+        return sequence
+    } else  {
+        return sequence;
+    }
 }
