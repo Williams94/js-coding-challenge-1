228c214f0e7eee7640de08e2eae18c9fde4ed41b
Ross <ross@Ross-W-MacBook-Pro.local>
2017-09-16 18:00:15 +0100

    Finished test 5

--- app/test2.js
+++ app/test2.js
@@ -1,17 +1,11 @@
 export function capitalise(names) {
-    return names.map(function (name) {
-        return name.toUpperCase();
-    });
+    return names.map(name => name.toUpperCase());
 }
 
 export function extractValue(objects, key) {
-    return objects.map(function (obj) {
-        return obj[key];
-    });
+    return objects.map(obj => obj[key]);
 }
 
 export function extractCompoundValue(objects, keysString) {
-    return objects.map(function (obj) {
-        return obj[keysString.split('.')[0]][keysString.split('.')[1]]
-    });
+    return objects.map(obj => obj[keysString.split('.')[0]][keysString.split('.')[1]]);
 }
