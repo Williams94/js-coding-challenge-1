6129868b4ef286307fefa11d5a407a27793e9c59
Ross <ross@Ross-W-MacBook-Pro.local>
2017-09-17 10:14:06 +0100

    Finished test 9

--- app/test9.js
+++ app/test9.js
@@ -1,3 +1,17 @@
 export default function countNodes(root) {
+  let count = 0;
 
+  if (root !== null || root !== undefined) {
+    count++;
+  }
+
+  if (root.left !== null && root.left !== undefined) {
+    count += countNodes(root.left);
+  }
+
+  if (root.right !== null && root.right !== undefined) {
+    count += countNodes(root.right);
+  }
+
+  return count;
 }
