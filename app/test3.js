ad128425cd0057ab6836c189a48b2a89bcd82341
Ross <ross@Ross-W-MacBook-Pro.local>
2017-09-17 10:52:07 +0100

    Completed test 3

--- app/test3.js
+++ app/test3.js
@@ -3,6 +3,9 @@ export default function test3(numMax) {
   if (numMax !== undefined && numMax !== 0) {
     for (let i = 0; i < numMax; i++) {
       sequence.push(i);
+      if (i !== 0 && i !== 1) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+      }
     }
     return sequence
   } else {
